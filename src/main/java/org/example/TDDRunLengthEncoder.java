cfb0a6a68d08b06dabc482b20c3b49fbd9421e5f
Jannik.Graf <jannik.graf@stud.uni-due.de>
2024-12-02 16:50:11 +0100

    testReturnData() läuft testEncodeValueLength1() in Arbeit

--- src/main/java/org/example/TDDRunLengthEncoder.java
+++ src/main/java/org/example/TDDRunLengthEncoder.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class TDDRunLengthEncoder {
+    public static int[] encode(int[] data) {
+        int[] encodedValue = {};
+        if (data.length == 0){
+            return encodedValue;
+        }
+        return data;
+    }
 }
--- src/test/java/TestCasesTDDrunLengthEncoder.java
+++ src/test/java/TestCasesTDDrunLengthEncoder.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.example.TDDRunLengthEncoder;
+
 public class TestCasesTDDrunLengthEncoder {
+    @Test
+    void testReturnData(){
+        int[] TestArray={};
+        int[] expected ={};
+        Assertions.assertArrayEquals(expected,TDDRunLengthEncoder.encode(TestArray));
+    }
+    @Test
+    void testEncodeValueLength1(){
+        int[] TestArray={2};
+        int[] expected ={2,1};
+        Assertions.assertArrayEquals(expected,TDDRunLengthEncoder.encode(TestArray));
+    }
+
+
 }
